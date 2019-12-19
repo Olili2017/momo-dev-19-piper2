680442abea065a7eaca7c9c1db8df657042bb704
Olili2017 <olilidaniel48@gmail.com>
2019-12-19 11:02:53 +0300

    (ANDROID/student) : finish student interface

---
+++ android/app/src/main/java/com/piper2/momo/android/digitalbursar/Transactions.java
@@ -0,0 +1,35 @@
+package com.piper2.momo.android.digitalbursar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.piper2.momo.android.digitalbursar.tools.RotateText;
+import com.piper2.momo.student.MainActivity;
+
+public class Transactions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transactions);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        TextView appName = findViewById(R.id.appname);
+        RotateText.build(appName, Transactions.this);
+
+    }
+
+    public static void startActivity(Context context){
+        Intent intent = new Intent(context, Transactions.class);
+        context.startActivity(intent);
+    }
+}
